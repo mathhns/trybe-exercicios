dd66c233425b1339b103d56743e03cffa44a85eb
Matheus Nunes da Silva <matheusnunessilva17@gmail.com>
2023-05-02 19:26:24 -0300

    02

--- secao-3/dia1-javascript/scripts.js
+++ secao-3/dia1-javascript/scripts.js
@@ -1,9 +1,8 @@
-let a = 6;
-let b = 3;
+let a = 10;
+let b = 22;
 //
-console.log('Soma: ' + (a + b));
-console.log('Subtração: ' + (a - b));
-console.log('Multiplicação: ' + (a * b));
-console.log('Divisão: ' + (a / b));
-console.log('Módulo: ' + (a % b));
-
+if (a > b) {
+  console.log('"a" é maior que "b".');
+} else {
+console.log('"b" é maior que "a".');
+}
