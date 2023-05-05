b2c4f8fd010fc4ef27d8d22e158bc0b7523df6cd
Matheus Nunes da Silva <matheusnunessilva17@gmail.com>
2023-05-05 12:00:28 -0300

    Impares

--- secao-3/dia2-javascript/scripts.js
+++ secao-3/dia2-javascript/scripts.js
@@ -1,5 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let index = 0; index < numbers.length; index +=1){
-console.log(numbers[index]);
+let oddNumber = [];
+
+for (let index = 0; index < numbers.length; index += 1){
+  if(numbers[index] % 2 !== 0){
+    oddNumber.push(numbers[index]);
+  }
+}
+if (oddNumber.length === 0){
+  console.log('Nenhum número ímpar encontrado');
+} else {
+  console.log(oddNumber.length);
 }
