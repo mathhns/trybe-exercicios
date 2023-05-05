8e099491572b7591208bd24e0e4a028bbb5077a9
Matheus Nunes da Silva <matheusnunessilva17@gmail.com>
2023-05-05 11:23:37 -0300

    01

--- secao-3/dia2-javascript/scripts.js
+++ secao-3/dia2-javascript/scripts.js
@@ -1,5 +1,5 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-menu.push('Contato')
-
-console.log(menu);
+for(let index = 0; index < numbers.length; index +=1){
+console.log(numbers[index]);
+}
