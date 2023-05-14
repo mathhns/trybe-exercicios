0fb425704ed624aec7de74ab380335b16eab335a
Matheus Nunes da Silva <matheusnunessilva17@gmail.com>
2023-05-13 23:23:45 -0300

    Concluindo exercícios

--- secao-3/dia4-javascript/scripts.js
+++ secao-3/dia4-javascript/scripts.js
@@ -1,33 +1,52 @@
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+let order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
 };
-//console.log('Bem-vinda, '+ info.personagem);
 
-info.recorrente = 'Sim';
-//console.log(info);
-
-for (let index in info) {
-  //console.log(info);
-};
-
-for (let index in info) {
-  //console.log(info[index]);
-};
-
-let info2 = {
-  personagem: 'Tio Patinhas',
-  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
-  nota: 'O último MacPatinhas',
-  recorrente: 'Sim',
-};
-for (let index in info) {
-  if (index === 'recorrente' && info[index] === 'Sim' && info2[index] === 'Sim') {
-    console.log('Ambos recorrentes');
-  } else {
-    console.log(info[index] + ' e ' + info2[index]);
-  }
+function customerInfo(order) {
+  return 'Olá ' + order.order.delivery.deliveryPerson + ', entrega para: ' + order.name + ', Telefone:' + order.phoneNumber + ', Endereço: ' + order.address.street + ', N°: ' + order.address.number + ' AP: ' + order.address.apartment;
 }
 
+//console.log(customerInfo(order));
 
+function orderModifier(order) {
+  let newBuyer = order.name = 'Luiz Silva';
+  let newTotal = order.payment.total = '50';
+  let pizzas = Object.keys(order.order.pizza);
+  let drinks = order.order.drinks.coke.type;
+
+  return 'Olá ' + newBuyer + ', o total do seu pedido de ' + pizzas[0] + ', ' + pizzas[1] + ' e ' + drinks + ' é R$ ' + newTotal + ',00.';
+}
+
+console.log(orderModifier(order));
