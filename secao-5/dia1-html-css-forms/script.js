6f414df1390d9efd4d72972ef65d5402d316ee0a
Matheus Nunes da Silva <matheusnunessilva17@gmail.com>
2023-05-26 00:36:22 -0300

    Exercícios Concluídos

--- secao-5/dia1-html-css-forms/script.js
+++ secao-5/dia1-html-css-forms/script.js
@@ -2,3 +2,20 @@
 const inputText = document.querySelector("#input-text");
 const inputCheckbox = document.querySelector("#input-checkbox");
 const hrefLink = document.querySelector("#href");
+
+//1
+href.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
+//2
+inputCheckbox.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
+//3 
+inputText.addEventListener('keypress', (event) => {
+  if (event.key !== 'a') {
+    event.preventDefault();
+  }
+});
