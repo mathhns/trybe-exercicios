f9ebcee3c66e1be1499cbdff73dffb2c29b66531
Matheus Nunes da Silva <matheusnunessilva17@gmail.com>
2023-06-01 05:21:21 -0300

    Aprofundando um pouco mais

--- secao-6/dia1-testes-unitarios/script.js
+++ secao-6/dia1-testes-unitarios/script.js
@@ -1,14 +1,13 @@
-const sum = (value1, value2) => {
-  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-    value1 = parseFloat(value1);
-    value2 = parseFloat(value2);
-
-    if (isNaN(value1) || isNaN(value2)) {
-      return 'Ambos os valores devem ser números válidos.';
-    }
-  }
-
-  return value1 + value2;
-};
-
-console.log(sum(2, '3'));
+// Primeira parte
+const promo = [
+  {product: 'bola de beach tennis', price: 29.99},
+  {product: 'mochila de trilha 20 litros', price: 120.99},
+  {product: 'capacete de ciclista', price: 180.59},
+  {product: 'toalha compacta de secagem', price: 39.99},
+  {product: 'kit 5 camisetas Dry Fit', price: 99.99},
+  {product: 'caneleiras de peso - 3kg (par)', price: 44.99},
+  {product: 'bola de ginástica e pilates', price: 149.59},
+  {product: 'óculos de natação', price: 18.99},
+  {product: 'capa protetora de prancha de surf', price: 89.59},
+  {product: 'bola de basquete', price: 39.99},
+];
