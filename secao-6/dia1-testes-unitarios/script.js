811a4643281f57c0c76dc8bc074efdb919e80ad1
Matheus Nunes da Silva <matheusnunessilva17@gmail.com>
2023-06-02 19:42:40 -0300

    Usando o fluxo de exceção

--- secao-6/dia1-testes-unitarios/script.js
+++ secao-6/dia1-testes-unitarios/script.js
@@ -11,3 +11,57 @@ const promo = [
   {product: 'capa protetora de prancha de surf', price: 89.59},
   {product: 'bola de basquete', price: 39.99},
 ];
+
+const button = document.querySelector('#send-button');
+
+button.addEventListener('click', () => {
+  const name = document.querySelector('#name-id').value;
+  const number = document.querySelector('#number-id').value;
+
+  showPromo(name, number);
+});
+
+const showPromo = (name, number) => {
+  const firstText = document.querySelector('#text-initial');
+  const secondText = document.querySelector('#text-final');
+  
+  try {
+    checkName(name);
+    checkNumber(parseInt(number));
+    checkValidRange(number);
+    const productObject = checkPromo(number);
+
+    firstText.innerHTML = `Boas-vindas, ${name}!`;
+    secondText.innerHTML = `A promoção do dia é: ${productObject.product} no valor de R$ ${productObject.price}`;
+  } catch(error) {
+    secondText.innerHTML = error.message;
+  }
+};
+
+const checkName = (name) => {
+  let letters = /[aA-zZ]+/;
+  
+  if (!name.match(letters)) {
+    throw new Error('É necessário digitar um nome válido');
+  }
+};
+
+const checkNumber = (number) => {
+  if (isNaN(number)) {
+    throw new Error('É necessário digitar um número');
+  }
+};
+
+const checkPromo = (number) => {
+  for (let index = 0; index < promo.length; index += 1) {
+    if (number - 1 === index) {
+      return promo[index];
+    }
+  }
+};
+
+const checkValidRange = (number) => {
+  if (number < 1 || number > 10) {
+    throw new Error('É necessário digitar um número entre 1 e 10');
+  }
+};
